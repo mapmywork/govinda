5a0c07d447cefeaabbb2d85e7bba2ad6331bba29
iamprince8 <vision.iamprince@gmail.com>
2026-07-11 00:55:17 +0530

    all done

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -37,7 +37,7 @@ export default function Navbar() {
       <div className="container mx-auto px-4 md:px-6 flex items-center justify-between">
         {/* Logo */}
         <a href="/" className="flex items-center gap-2 z-50 relative">
-          <img src={logoImg} alt="Govinda Go Logo" className="h-24 lg:h-32 object-contain rounded-md" />
+          <img src={logoImg} alt="Govinda Go Logo" className="h-16 lg:h-20 object-contain rounded-md" />
         </a>
 
         {/* Desktop Nav */}
